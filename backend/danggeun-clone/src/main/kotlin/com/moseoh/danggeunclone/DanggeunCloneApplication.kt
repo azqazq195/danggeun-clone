5ddc4eb45a8a76ccadb51936abe14bad6a0e25ae
moseoh <azqazq195@gmail.com>
2023-06-07 18:23:05 +0900

    feat: init

---
+++ backend/danggeun-clone/src/main/kotlin/com/moseoh/danggeunclone/DanggeunCloneApplication.kt
@@ -0,0 +1,11 @@
+package com.moseoh.danggeunclone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DanggeunCloneApplication
+
+fun main(args: Array<String>) {
+    runApplication<DanggeunCloneApplication>(*args)
+}
