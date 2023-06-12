1cd6c916051c0725d939aebb63c03e8b0820a8a5
moseoh <azqazq195@gmail.com>
2023-06-13 05:56:18 +0900

    refactor: Validation 추가

--- backend/src/main/kotlin/com/moseoh/danggeunclone/post/application/dto/UpdatePostRequest.kt
+++ backend/src/main/kotlin/com/moseoh/danggeunclone/post/application/dto/UpdatePostRequest.kt
@@ -1,10 +1,18 @@
 package com.moseoh.danggeunclone.post.application.dto
 
 import com.moseoh.danggeunclone.post.domain.Status
+import jakarta.validation.constraints.NotBlank
 
 data class UpdatePostRequest(
+    @field:NotBlank
     val title: String?,
+
+    @field:NotBlank
     val category: String?,
+
+    @field:NotBlank
     val content: String?,
+
+    @field:NotBlank
     val status: Status?
 )
