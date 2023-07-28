e5254aab4050f19cd0435e4c8180ad94677632eb
moseoh <azqazq195@gmail.com>
2023-07-28 14:00:45 +0900

    init

---
+++ danggeun-server/src/main/kotlin/com/moseoh/danggeunserver/DanggeunServerApplication.kt
@@ -0,0 +1,11 @@
+package com.moseoh.danggeunserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DanggeunServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<DanggeunServerApplication>(*args)
+}
