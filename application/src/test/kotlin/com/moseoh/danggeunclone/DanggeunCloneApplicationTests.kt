2f5a820f5af80719a1e42ff4d11e7a38ba9fb24a
moseoh <azqazq195@gmail.com>
2023-07-28 14:08:11 +0900

    init

---
+++ application/src/test/kotlin/com/moseoh/danggeunclone/DanggeunCloneApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.moseoh.danggeunclone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DanggeunCloneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
