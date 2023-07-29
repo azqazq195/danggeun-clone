c00bce541a8bc16da99e67508dfe64ae975a2186
moseoh <azqazq195@gmail.com>
2023-07-29 19:16:03 +0900

    refactor: 하위 모듈 Scan 추가

--- application/module-api/src/main/kotlin/com/moseoh/danggeunclone/api/DanggeunCloneApplication.kt
+++ application/module-api/src/main/kotlin/com/moseoh/danggeunclone/api/DanggeunCloneApplication.kt
@@ -1,11 +1,27 @@
 package com.moseoh.danggeunclone.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
+@ComponentScan("com.moseoh.danggeunclone")
+@EntityScan("com.moseoh.danggeunclone.core")
 class DanggeunCloneApplication
 
 fun main(args: Array<String>) {
     runApplication<DanggeunCloneApplication>(*args)
+}
+
+@RestController
+class HealthCheckController {
+    @GetMapping("/health")
+    fun healthCheck(): ResponseEntity<Void> {
+        return ResponseEntity.status(HttpStatus.OK).build()
+    }
 }
