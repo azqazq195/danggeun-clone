30e65d97f23de74fc45cc3be419b4aea580ccddf
moseoh <azqazq195@gmail.com>
2023-08-08 18:57:04 +0900

    refactor: jpa, redis repository 설정 추가

--- application/module-api/src/main/kotlin/com/moseoh/danggeunclone/api/DanggeunCloneApplication.kt
+++ application/module-api/src/main/kotlin/com/moseoh/danggeunclone/api/DanggeunCloneApplication.kt
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController
 @ComponentScan("com.moseoh.danggeunclone")
 @EntityScan("com.moseoh.danggeunclone.core")
 @ConfigurationPropertiesScan("com.moseoh.danggeunclone")
+@EnableJpaRepositories("com.moseoh.danggeunclone")
+@EnableRedisRepositories("com.moseoh.danggeunclone")
 class DanggeunCloneApplication
 
 fun main(args: Array<String>) {
