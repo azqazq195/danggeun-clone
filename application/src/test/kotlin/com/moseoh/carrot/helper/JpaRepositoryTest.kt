306fd3fc00c1a0c7bafac2f1d0f30698317609ad
moseoh <azqazq195@gmail.com>
2023-08-17 17:56:01 +0900

    style(app): format 수정

--- application/src/test/kotlin/com/moseoh/carrot/helper/JpaRepositoryTest.kt
+++ application/src/test/kotlin/com/moseoh/carrot/helper/JpaRepositoryTest.kt
@@ -6,8 +6,6 @@ import io.kotest.core.spec.style.BehaviorSpec
 @DataJpaTestWithConfig
 abstract class JpaRepositoryTest(
     body: BehaviorSpec.() -> Unit = {},
-) : BehaviorSpec() {
-    init {
-        body()
-    }
-}
+) : BehaviorSpec({
+    body()
+})
