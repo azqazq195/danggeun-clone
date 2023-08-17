2ac14fa5f30351c47e90910d6e0ee195c714d0b7
moseoh <azqazq195@gmail.com>
2023-08-17 11:21:40 +0900

    test(app): @SpringBootTest에 @Transactional 추가

--- application/src/test/kotlin/com/moseoh/carrot/helper/constraints/SpringBootTestWithConfig.kt
+++ application/src/test/kotlin/com/moseoh/carrot/helper/constraints/SpringBootTestWithConfig.kt
@@ -2,9 +2,11 @@ package com.moseoh.carrot.helper.constraints
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
 @SpringBootTest
 @ConfigurationPropertiesScan("com.moseoh.carrot._common.environment")
+@Transactional
 annotation class SpringBootTestWithConfig
