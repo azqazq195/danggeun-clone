c6f17c3dadf8eed13168a6e557d6b241d4eb4105
moseoh <azqazq195@gmail.com>
2023-08-17 18:00:26 +0900

    chore(app): Docker 구성 변경 - application 도 구성에 추가. - Scale-Out 가능하도록 작성.

--- application/script/docker-up.sh
+++ application/script/docker-up.sh
@@ -3,4 +3,7 @@
 # -f docker-compose 파일 위치 설정
 # -p Project Name 설정
 # -d 백그라운드 실행
-docker-compose -f ./docker/docker-compose.yml -p carrot up -d
+# --scale container 갯수 설정
+#./gradlew build
+
+docker-compose -f ./docker/docker-compose.yml -p carrot up -d --scale app=2
