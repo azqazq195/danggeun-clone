0eaf3a07fa24ece7a5cb6b2c437121ececf028d8
moseoh <azqazq195@gmail.com>
2023-09-15 16:59:00 +0900

    test: arm64 cpu 용 embedded redis 설정 추가

--- application/src/test/kotlin/com/moseoh/carrot/helper/config/EmbeddedRedisConfig.kt
+++ application/src/test/kotlin/com/moseoh/carrot/helper/config/EmbeddedRedisConfig.kt
@@ -1,11 +1,18 @@
 package com.moseoh.carrot.helper.config
 
+import io.lettuce.core.RedisURI
 import jakarta.annotation.PostConstruct
 import jakarta.annotation.PreDestroy
+import org.springframework.core.io.ClassPathResource
 import redis.embedded.RedisServer
+import java.io.File
 
 class EmbeddedRedisConfig {
-    private val redisServer: RedisServer = RedisServer()
+    private val redisServer: RedisServer =
+        if (isArmMac()) RedisServer(
+            getRedisFileForArmMac(),
+            RedisURI.DEFAULT_REDIS_PORT
+        ) else RedisServer()
 
     @PostConstruct
     fun startRedis() {
@@ -15,5 +22,14 @@ class EmbeddedRedisConfig {
     @PreDestroy
     fun stopRedis() {
         redisServer.stop()
+    }
+
+    private fun isArmMac(): Boolean {
+        return System.getProperty("os.arch") == "aarch64" &&
+                System.getProperty("os.name") == "Mac OS X"
+    }
+
+    private fun getRedisFileForArmMac(): File {
+        return ClassPathResource("redis-server-6.2.13-mac-arm64").file
     }
 }
